b52d3135c513477a09b54cbea2b4e8eb438b6e15
FernandoRodriguezG <fejerogo.17@gmail.com>
2023-03-29 19:18:20 -0600

    cambios

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
 import React, {useEffect, useState} from 'react';
-import logo from './logo.svg';
+import profilePic from './ff.jpeg';
 import './App.css';
-import { 
-  FaGithub,
-  FaInstagram,
-  FaTelegram,
-  FaTwitter,
-  FaYoutube
-} from 'react-icons/fa';
 import { createClient } from "@supabase/supabase-js";
 import Icon from './Icon';
 
@@ -37,22 +30,28 @@ function App() {
   }
 
   return (
-    <div className="App">
+    <div className="App text-white">
       <div className="App-header container mx-auto">
-        <img src={logo} className="profile-pic w-20 h-20 mb-4 rounded-full" alt="logo" />
+        <img src={profilePic} className="profile-pic w-32 h-32 mb-4 rounded-full" alt="logo" />
         {
           loading ? <p>Cargando...</p> : 
-          <ul className='flex flex-col w-full'>
-          {
-            links && links.map((link, index) => (
-              <li key={index} className='cursor-pointer m-2 py-3 px-10 border-black border-2 text-sm md:text-md text-slate-90'>
-                <a href={link.url} className='flex items-center justify-center' title={link.title} target='_blank' rel='noreferrer'>
-                <Icon iconName={link.icon}/>  <p className='ml-3'>{link.title}</p>
-                </a>
-              </li>
-            ))
-          }
-        </ul>
+          <div className='container'>
+            <p>Fernando Rodríguez González</p>
+            <p className='text-sm m-3'>
+              23 años. Ingeniero en Sistemas Inteligentes por la UASLP con especialidad en desarrollo web y multiplataforma.
+            </p>
+            <ul className='flex flex-col w-full'>
+              {
+                links && links.map((link, index) => (
+                  <li key={index} className='cursor-pointer m-2 py-3 px-10 border-white border-2 text-sm md:text-md text-slate-90'>
+                    <a href={link.url} className='flex items-center justify-center' title={link.title} target='_blank' rel='noreferrer'>
+                    <Icon iconName={link.icon}/>  <p className='ml-3'>{link.title}</p>
+                    </a>
+                  </li>
+                ))
+              }
+            </ul>
+          </div>
         }
         
       </div>
